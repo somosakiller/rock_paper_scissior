4d0825b6acbc99606f6708db04fe1e65ed1decaf
Saad <saad09@msn.com>
2022-03-15 19:28:21 -0400

    updated RPS code with basic GUI

--- RPS.js
+++ RPS.js
@@ -1,3 +1,32 @@
+let round="";
+let div = document.createElement('div');
+let container = document.querySelector('.results');
+const btn = document.querySelectorAll('button');
+let playerWinCount=0;
+let aiWinCount= 0;
+let tieCount=0;
+let roundCounter = 0;
+
+let clickButton = ()=>{
+    btn.forEach(button =>{
+        button.addEventListener('click', ()=>{
+            if (roundCounter<5){
+                //play one round
+                let result = playRound(button.textContent);
+                //send result to counter function
+                resultCounter(result);
+                //display results in div
+                divDisplay(playerWinCount,aiWinCount);
+                roundCounter++;
+            }
+            if (roundCounter==5) {
+                divWinner(playerWinCount,aiWinCount);
+            }
+        })
+    })
+}
+clickButton();
+
 let computerPlay = () => {
     let move = ['rock', 'paper', 'scissor'];
     return move[Math.floor(Math.random()*move.length)];
@@ -9,56 +38,64 @@ let playRound = (choice) => {
     const computerSelection = computerPlay();
 
     if ((choice.toLowerCase() == 'rock') && (computerSelection.toLocaleLowerCase() == 'paper')) {
-        return 'You lose! Paper beats rock.'
+        return 'You lose! Paper beats rock.';
     }
 
     if ((choice.toLowerCase() == 'rock') && (computerSelection.toLocaleLowerCase() == 'scissor')) {
-        return 'You win! rock beats scissor.'
+        return 'You win! rock beats scissor.';
     }
 
     if ((choice.toLowerCase() == 'paper') && (computerSelection.toLocaleLowerCase() == 'scissor')) {
-        return 'You lose! scissor beats paper.'
+        return 'You lose! scissor beats paper.'; 
     }
 
     if ((choice.toLowerCase() == 'paper') && (computerSelection.toLocaleLowerCase() == 'rock')) {
-        return 'You win! paper beats rock.'
+        return 'You win! paper beats rock.'; 
     }
 
     if ((choice.toLowerCase() == 'scissor') && (computerSelection.toLocaleLowerCase() == 'paper')) {
-        return 'You win! scissor beats paper.'
+        return 'You win! scissor beats paper.';
     }
 
     if ((choice.toLowerCase() == 'scissor') && (computerSelection.toLocaleLowerCase() == 'rock')) {
-        return 'You lose! rock beats scissor.'
+        return 'You lose! rock beats scissor.';
     }
 
     if (choice.toLowerCase() == computerSelection.toLocaleLowerCase()) {
-        return 'You tie!'
+        return 'You tie!';
     }
 
 }
 
-let promptUser = () => {
-    let decision = prompt('enter choice');
-    return playRound(decision);
+let resultCounter = (result) => {
+    if (result.includes('win')) {
+        playerWinCount+=1;
+    }
+    if (result.includes('lose')) {
+        aiWinCount+=1;
+    }
+    if (result.includes('tie')) {
+        tieCount+=1;
+    }
 }
 
+let divDisplay = (playerWinCount,aiWinCount) =>{
+    console.log(div);
+    div.textContent = `Wins: ${playerWinCount}    Loss: ${aiWinCount}    Tie:${tieCount} `; 
+    container.appendChild(div);
+}
 
-
-let game = () => {
-    let playerWinCount=0;
-    let aiWinCount= 0;
-    for (let i=0; i<5; i++){
-        let round = promptUser();
-        if (round.includes('win')) {
-            playerWinCount+=1;
-        }
-        if (round.includes('lose')) {
-            aiWinCount+=1;
-        }
-
+let divWinner = (playerWinCount, aiWinCount) =>{
+    if(playerWinCount>aiWinCount){
+        div.textContent = `Player Won`;
+        container.appendChild(div);
     }
-    
-    return 'player won ' + playerWinCount + ' times and AI won ' + aiWinCount + ' times';
-     
+    else if (aiWinCount>playerWinCount){
+        div.textContent = `AI Won`;
+        container.appendChild(div); 
+    }
+    else {
+        div.textContent = `You Tie`;
+        container.appendChild(div); 
+    }
 }
